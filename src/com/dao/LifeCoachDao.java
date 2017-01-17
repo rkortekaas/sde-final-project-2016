71df8922400fdfc514830f21508a6959c2bb2b82
Remco Kortekaas <rackortekaas@gmail.com>
2017-01-17 23:41:28 +0100

    last commit

---
+++ src/com/dao/LifeCoachDao.java
@@ -0,0 +1,40 @@
+package com.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public enum LifeCoachDao {
+    
+    private EntityManagerFactory emf;
+
+    private LifeCoachDao() {
+        if (emf!=null) {
+            emf.close();
+        }
+        emf = Persistence.createEntityManagerFactory("assignment");
+    }
+
+    public EntityManager createEntityManager() {
+        try {
+            return emf.createEntityManager();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;    
+    }
+
+    public void closeConnections(EntityManager em) {
+        em.close();
+    }
+
+    public EntityTransaction getTransaction(EntityManager em) {
+        return em.getTransaction();
+    }
+
+    public EntityManagerFactory getEntityManagerFactory() {
+        return emf;
+    }  
+}
